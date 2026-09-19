9fecee164be281c42aa1e0008151c050f1eed578
Ashim Aryal <ashimaryal25@gmail.com>
2026-09-19 14:49:40 -0400

    Time: 5 ms (29.57%), Space: 12.3 MB (91.74%) - LeetHub

--- 0948-bag-of-tokens/0948-bag-of-tokens.py
+++ 0948-bag-of-tokens/0948-bag-of-tokens.py
@@ -1,35 +1,28 @@
 class Solution(object):
     def bagOfTokensScore(self, tokens, power):
-        
-
-        sorted_tokens = sorted(tokens)
-
-        curPower = power
-
+        p = power
         score = 0
+        length = len(tokens)
 
+        q = deque(sorted(tokens))
 
-        # two pointers on a sorted list = deque
-
-        #facedown u would want to pick the max fromt the token array from the token array so we can probebly use maxheap to put the max at the left. for faceup goal is to maximise the score so youd want to strategeciclly choose the lowest tokens from the tokens array.
-
-        left = 0
-
-        right = len(sorted_tokens) - 1
-
-        # When left == right, there is still 1 token left. If you have enough power, can and should buy it
-        while left <= right:
-
-            if sorted_tokens[left] <= curPower:
-                curPower -= sorted_tokens[left]
+        # if facedown at last dont play
+        while q:
+            
+            if p >= q[0] :
+                p -= q.popleft()
                 score += 1
-                left = left + 1
-            elif score > 0 and right-left+1 > 1:
-                curPower += sorted_tokens[right]
-                score -= 1    
-                right = right - 1
             else:
-                break    
+                if score == 0:
+                    break
+
+                if len(q) == 1:
+                    break 
+
+                score -= 1
+                p += q.pop()
         return score
+
+
 
         
