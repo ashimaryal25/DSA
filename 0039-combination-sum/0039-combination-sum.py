8c374227d6ec44f9c5f7e4c896165d60b4e2f0f5
Ashim Aryal <ashimaryal25@gmail.com>
2026-09-19 17:23:48 -0400

    Time: 20 ms (38.06%), Space: 12.5 MB (56.62%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,20 +1,19 @@
 class Solution(object):
     def combinationSum(self, candidates, target):
         res = []
-        
-        def dfs(start_index, current_combo, current_sum):
+        stack = []
+
+        def dfs(start_index, current_sum):
             if current_sum == target:
-                res.append(list(current_combo))
+                res.append(list(stack))
                 return
-            
+
             if current_sum > target:
                 return
-            
+                   
             for i in range(start_index, len(candidates)):
-                current_combo.append(candidates[i])
-                dfs(i, current_combo, current_sum + candidates[i])
-                current_combo.pop()
-
-        dfs(0, [], 0)
+                stack.append(candidates[i])
+                dfs(i, current_sum + candidates[i])
+                stack.pop()
+        dfs(0, 0)
         return res
-        
