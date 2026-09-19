91cd9e98acd004e143296ef54284be77eacca4b6
Ashim Aryal <ashimaryal25@gmail.com>
2026-09-19 15:46:09 -0400

    Time: 2 ms (14.4%), Space: 12.6 MB (25.22%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -4,13 +4,14 @@ class Solution(object):
         stack = []
 
         def dfs(start_index):
+
             res.append(list(stack))
 
             for i in range(start_index, len(nums)):
-                stack.append(nums[i])
-                dfs(i + 1)
-                stack.pop()
 
-        dfs(0)
+                stack.append(nums[i])
+                dfs(i+1)
+                stack.pop()
+        dfs(0)        
         return res
     
